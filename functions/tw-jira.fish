9e271933cd90995d779fd148e4dfc9c7ec2ca4e0
Erik Samuelsson <erik.samuelsson@northvolt.com>
2022-05-02 13:50:19 +0200

    Remake tw functions

--- functions/tw-jira.fish
+++ functions/tw-jira.fish
@@ -1,11 +1,22 @@
-function tj
-    jira open (task _get $argv.jira_id)
-end
 
 function get_new_jira
-    jira issue list -a(jira me) | grep -v (get_tw_active_jira) | awk '{ print $2 " jira_id:" $1 }' FS='\t'
+    jira issue list -a (jira me) --plain | grep -v (get_tw_active_jira) | awk '{print $2}'
 end
 
 function get_tw_active_jira
     t jira | awk '/DADVG.+/' | sed 'N;s/\n/\\|/g'
+end
+
+function tj
+    jira open (task _get $argv.jira_id)
+end
+
+function ts
+    watson start (t _get $argv.description) + (t _get $argv.jira_id)
+    t start $argv
+end
+
+function tst
+    watson stop
+    t stop $argv
 end
