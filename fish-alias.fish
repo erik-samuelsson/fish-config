921252edc27b70c7a78c443bce47f275c2aab17b
Erik Samuelsson <erik.samuelsson@northvolt.com>
2022-09-01 15:56:02 +0200

    update fish alias

---
+++ fish-alias.fish
@@ -0,0 +1,21 @@
+alias pwdc="pwd | pbcopy"
+alias t="task"
+alias ta="task add"
+alias td="task done"
+alias tm="task modify"
+alias looker="open -a Safari https://northvolt.cloud.looker.com/browse"
+
+
+alias confluence="open https://northvolt.atlassian.net/wiki/home"
+alias awsapps="open https://northvolt.awsapps.com/start#/"
+
+alias lg="lazygit"
+
+alias synctw="python3.9 /Users/esamuelsson/Projects/Development/PythonTaskwarrior/pythontaskwarrior/sync.py"
+alias valghub="open https://github.com/northvolt/validation-data-analysis"
+alias jukit_kitty="kitty --listen-on=unix:@"(date +%s%N)" -o allow_remote_control=yes"
+alias issue="jira issue create --template ~/.templates/dadvg.md"
+alias ithelp="open https://northvolt.atlassian.net/servicedesk/customer/portal/5/group/222"
+alias access="open https://myaccess.microsoft.com/#/access-packages"
+alias ghvalibrary="open https://github.com/northvolt/validation-data-analysis"
+alias timerep="open -a Safari 'https://northvolt-prod.operations.dynamics.com/?cmp=se10&mi=TSTimesheetEntryGridViewMyTimesheets'"
