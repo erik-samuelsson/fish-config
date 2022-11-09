1da8a99faaa4d1c4bf68f72ffa012f53dfeb8f51
Erik Samuelsson <erik.samuelsson@northvolt.com>
2022-11-09 23:17:53 +0100

    fix buggy env variables

--- config.fish
+++ config.fish
@@ -6,6 +6,7 @@ source ~/.config/fish/fish-alias.fish
 source ~/.config/fish/functions/prompt.fish
 source ~/.config/fish/functions/redefine_watson.fish
 source ~/.config/fish/functions/tw-jira.fish
+source ~/.config/fish/functions/aws_funcs.fish
 
 set -xg valibrary ~/Projects/Development/validation-data-analysis
 set -xg JIRA_API_TOKEN GjId8kVhmsamALd2Ijw5B834
@@ -14,5 +15,10 @@ set -xg EMAIL erik.samuelsson@northvolt.com
 set -xg OneDrive "/Users/esamuelsson/Library/CloudStorage/OneDrive-northvolt.com/07_Customer Validation/"
 set -xg GPG_TTY (tty)
 set -xg DBT_PROFILES_DIR ~/Projects/Development/data-models
+set -xg NVIM_PYTHON_LOG_FILE ~/.config/nvim/log
 
-nvm use v17
+
+
+
+set -e fish_user_path 
+set -x fish_user_path $HOME/.serverless//bin/
