df048d5fdd7b0e6df0528b977498cf93ecd81044
Erik Samuelsson <erik.samuelsson@northvolt.com>
2024-03-03 15:17:08 +0100

    redshift login function for dbe

--- functions/aws_funcs.fish
+++ functions/aws_funcs.fish
@@ -13,6 +13,31 @@ function aws_redshift_login
 end
 #{\"url\": \"$DB_ANALYTICAL_RDS\", \"name\": \"rnd_rds\"}
 
+function rlogin
+  set AWS_PROFILE "nv-redshift-developer-datalake"
+  set RS_CLUSTER eu-north-1-aws-datalake-data-platform-redshift
+  set user developer
+  set RS_USER "IAM%3A$user"  # %3A = : encoding
+  set RS_HOST eu-north-1-aws-datalake-data-platform-redshift.czoml5r2boyw.eu-north-1.redshift.amazonaws.com
+  set RS_PORT 5439
+  set RS_DB datawarehouse
+
+  set PGPASS_REDSHIFT (aws redshift get-cluster-credentials --duration-seconds=3600 --profile="$AWS_PROFILE" --cluster-identifier="$RS_CLUSTER" --db-user="$user" | jq -r '.DbPassword' | tr -d \")
+  echo $PGPASS_REDSHIFT
+  set PGPASS_ENCODED (echo -n $PGPASS_REDSHIFT | jq -sRr @uri)
+  echo $PGPASS_ENCODED
+  set DB_REDSHIFT_DATAWAREHOUSE "postgres://$RS_USER:$PGPASS_ENCODED@$RS_HOST:$RS_PORT/$RS_DB?sslmode=require"
+
+  set -xg DBEE_CONNECTIONS '[
+  {
+    "name": "redshift-datawarehouse",
+    "url": "'$DB_REDSHIFT_DATAWAREHOUSE'",
+    "type": "redshift"
+  }
+]' 
+  echo $DBEE_CONNECTIONS > $HOME/.local/share/db_ui/connections.json
+end
+
 function awsredcon
   set -xg RS_CLUSTER eu-north-1-aws-datalake-data-platform-redshift
   set -xg PGPASSWORDRAW (aws --profile nv-datalake redshift get-cluster-credentials --cluster-identifier $RS_CLUSTER --db-user developer --duration-seconds 1800 | awk 'FNR==3 {print $2}' | tr -d \", )
