6508b8facb5fe62c2c03d164490399ffc6480ed1
Erik Samuelsson <erik.samuelsson@northvolt.com>
2024-03-03 15:18:28 +0100

    config updates

--- config.fish
+++ config.fish
@@ -11,6 +11,7 @@ source ~/.config/fish/functions/analytics.fish
 source ~/.config/fish/functions/go.fish
 source ~/.config/fish/functions/pyspark.fish
 source ~/.config/fish/functions/popuptmux.fish
+source ~/.config/fish/functions/find_delete_markers.fish
 
 
 set -xg valibrary ~/Projects/Development/validation-data-analysis
@@ -31,27 +32,38 @@ set -x fish_user_path $HOME/.serverless//bin/
 fish_add_path $HOME/.local/bin/ 
 set -xg OBSIDIAN $HOME/Documents/NV/
 
-set -xg FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git --exclude node_modules --exclude '*.rst*' --exclude '*.html' --exclude '*.doctree' --exclude '*.xlsx'"
+set -xg FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix --hidden --follow --exclude .git --include .github --exclude node_modules --exclude '*.rst*' --exclude '*.html' --exclude '*.doctree' --exclude '*.xlsx'"
 
 #set -xg FZF_DEFAULT_COMMAND rg --files -g '"!**.html|!**.rst*|!**.svg|!**.xlsx|!**.db"'
 
-set -xg OPENAI_API_KEY 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
+# set -xg OPENAI_API_KEY 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
+set -xg OPENAI_API_KEY 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
 
 # set -xg SPARK_HOME /usr/local
 
 
 # Northvolt standard setup
-set -x GOVERSION "go1.21.1"
+set -x GOVERSION "go1.21.5"
+
+
+set -xg GOPATH $HOME/go
 
 
 function nvdev
   source ~/src/github.com/northvolt/tools/etc/nvrc.fish
-  source ~/src/github.com/northvolt/tools/etc/nvrc.sh
+  # source ~/src/github.com/northvolt/tools/etc/nvrc.sh
   bass source ~/src/github.com/northvolt/tools/bin/git-global-config.sh
   #bass source ~/src/github.com/northvolt/nv-aws-sso-configs/aws-cli-helper-commands
 end
 
+function reset_goproxypass
+  aws sso login --profile=nv-automation-dev
+  set GOPROXY_PASS (aws --region eu-west-1 --profile=nv-automation-dev ssm get-parameter --with-decryption --name GOPROXY_PASS | jq -r '.Parameter.Value')
+  security add-generic-password -a $USER -s GOPROXY_PASS -w $GOPROXY_PASS
+end
 # Fix GPG issue for signing github commits
+
+# set -xg GPXP Yeeh8IZ2V661zWrv2urdowCvwkjbPSpU
 
 function awsdev
   set -xg AWS_PROFILE nv-automation-dev
